949d27fac1aa03d2ad15aca776be9d1beed6fb99
Przemysław Kuzia <przemek.kuzia@gmail.com>
2024-04-18 14:17:39 +0200

    Bump Mux do 7.1.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/THEOplayer/theoplayer-sdk-ios.git",
-            from: "6.13.0"
+            from: "7.1.1"
         )
     ],
     targets: [
